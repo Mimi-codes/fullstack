a0a76c0a267de65b9a245a84feb0fd13e4d505d8
Tomi <mariamismael904@gmail.com>
2023-05-20 00:43:05 +0100

    redux-anecdotes

--- part_6/redux-anecdotes/src/components/Notification.js
+++ part_6/redux-anecdotes/src/components/Notification.js
@@ -1,14 +1,29 @@
-const Notification = () => {
-  const style = {
-    border: 'solid',
-    padding: 10,
-    borderWidth: 1
-  }
-  return (
-    <div style={style}>
-      render here notification...
-    </div>
-  )
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Notification = (props) => {
+	const notification = props.notification
+	const style = {
+		border: 'solid',
+		padding: 10,
+		borderWidth: 1
+	}
+	if (notification === '') {
+		return <div></div>
+	} else {
+		return (
+			<div style={style}>
+				{notification}
+			</div>
+		)
+	}
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+	return {
+		notification: state.notification
+	}
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification

94cea540a3762e0d6b50f90fbbd100d798f65dbf
Tomi <mariamismael904@gmail.com>
2023-05-25 22:06:01 +0100

    bloglist extended

---
+++ part_7/bloglist_extended/server/models/user.js
@@ -0,0 +1,29 @@
+
+const mongoose = require("mongoose");
+
+//creates new user using schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog",
+    },
+  ],
+});
+
+//formats the returned object
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
